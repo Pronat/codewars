b691d2cf5a171527a96608e5dc80f561653874c6
AlexPron <Fotolinza@gmail.com>
2022-07-07 14:20:38 +0300

    1.3

--- src/codewars/1.ts
+++ src/codewars/1.ts
@@ -1,4 +1,6 @@
 
 export function mergeArrays(arr1: Array<number> , arr2: Array<number>) {
-    return arr1.concat(arr2).sort()
+    let uniqArr = Array.from(new Set(arr1.concat(arr2).sort((a, b) => a - b )))
+    return uniqArr
+
 }
