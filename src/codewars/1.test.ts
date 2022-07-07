06e7a093489101f2520bcf71745ff4918ac8486a
AlexPron <Fotolinza@gmail.com>
2022-07-07 13:12:59 +0300

    1.2

---
+++ src/codewars/1.test.ts
@@ -0,0 +1,7 @@
+import {mergeArrays} from "./1";
+
+test('merge array correct', ()=> {
+    let arr1 = [1,2,3,4]
+    let arr2 = [1,2,3,4,5,6,7,8,9,10]
+    mergeArrays
+})
---
+++ src/codewars/1.ts
@@ -0,0 +1,4 @@
+
+export function mergeArrays(arr1: Array<number> , arr2: Array<number>) {
+    return arr1.concat(arr2).sort()
+}
